6420c912e4531d7aed223f292de8bb4901d17429
Shohnur <Shohnur@gamil.com>
2024-06-22 12:20:05 +0500

    added new file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from dotenv import load_dotenv
-from default_button import menu_keyboard
+from default_button import menu_keyboard, menu_detail_keyboard
 from aiogram import Bot, Dispatcher, executor, types
 
 load_dotenv()
@@ -19,7 +19,12 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     username = message.from_user.username
-    await message.reply(f"Hello  @{username}",reply_markup=menu_keyboard)
+    await message.reply(f"Hello  @{username}", reply_markup=menu_keyboard)
+
+
+@dp.message_handler(lambda message: message.txt == "Menu")
+async def menu(message: types.Message):
+    await message.reply("Select one of the products  >>> ", reply_markup=menu_detail_keyboard)
 
 
 @dp.message_handler()
--- default_button.py
+++ default_button.py
@@ -3,3 +3,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 menu_keyboard.add(KeyboardButton("Menu"))
 
+menu_detail_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+menu_detail_keyboard.add(KeyboardButton("Product 1"), KeyboardButton("Product 2"))
